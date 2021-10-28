b011edc0fdb98fe9c5995513ccd5338d8c62c272
Nacho-Batista <batistanach@gmail.com>
2021-10-28 00:05:11 -0300

    Infinite Scroll

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,7 @@ import { HomeService } from './home.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  HomeCards: any[] =Array(10);
+  HomeCards: any[] =Array(2);
 public proyecto: any;
   constructor(private homeService: HomeService) {}
 
@@ -17,4 +17,20 @@ public proyecto: any;
         })
       }
 
+
+    loadData(event) {
+      setTimeout(() => {
+        console.log('Done');
+
+        var masHomeCards = Array(2);
+        this.HomeCards.push(...masHomeCards);
+        event.target.complete();
+  
+        // App logic to determine if all data is loaded
+        // and disable the infinite scroll
+        //if (data.length == 1000) {
+        //  event.target.disabled = true;
+        //}
+      }, 2000);
+    }  
 }
